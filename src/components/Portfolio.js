be046853f3e83916968d461e97ac519ea8163d64
Victoria00000 <vvictoriaandres@gmail.com>
2022-04-15 09:19:58 -0300

    commit

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -24,8 +24,8 @@ export const Portfolio = () => {
                   <div className="card-body">
                     <h5 className="card-title"> TrendingMovies </h5>
                     <p className="card-text"> DESCRIPCIONES dfsdfjsdojfosdj jfsdjfslkdjflsdkjf dlsjflksdjlfkj fjsljdfldskjf  dlaskjdlsakjd lkjljlkjljljlkjlkjlkjklj ljlkjlkjljlkjlkjljlkjljl  FRONTEND responsive proyect with REACT.js Consuming the API movieDb, with: "dependencies": "react": "^17.0.2", "react-dom": "^17.0.2", "react-icons": "^4.3.1", "react-infinite-scroll-component": "^6.1.0", "react-router-dom": "^6.2.1", "react-scripts": "5.0.0", </p>
-                    <a href="https://github.com/Victoria00000/MoviesHouse" target="_blank" className="btn btn-light mx-2" style={{ background: '#d63384', color: 'white' }}> GitHub </a>
-                    <a href="https://trendingmovieshouse.netlify.app/" target="_blank" className="btn btn-light mx-2" style={{ background: '#d63384', color: 'white' }}> Deploy </a>
+                    <a href="https://github.com/Victoria00000/MoviesHouse" target="_blank" rel='noopener no referrer' className="btn btn-light mx-2" style={{ background: '#d63384', color: 'white' }}> GitHub </a>
+                    <a href="https://trendingmovieshouse.netlify.app/" target="_blank" rel='noopener no referrer' className="btn btn-light mx-2" style={{ background: '#d63384', color: 'white' }}> Deploy </a>
                   </div>
                 </div>
 
@@ -35,8 +35,8 @@ export const Portfolio = () => {
                     <h5 className="card-title"> mySTORE </h5>
                     <p className="card-text"> FRONTEND proyect with REACT.JS consuming a public API (fakestoreapi.com).
                       And: "dependencies": "axios": "^0.26.1", "bootstrap": "^5.1.3", "font-awesome": "^4.7.0", "node-modules": "^1.0.1", "react": "^17.0.2", "react-dom": "^17.0.2", "react-loading-skeleton": "^3.0.3", "react-redux": "^7.2.6", "react-router": "^6.2.2", "react-router-dom": "^6.2.2", "react-scripts": "5.0.0", "redux": "^4.1.2"</p>
-                    <a href="https://github.com/Victoria00000/E-COMMERCE-APP" target="_blank" className="btn btn-light mx-2" style={{ background: '#d63384', color: 'white' }}> GitHub </a>
-                    <a href="https://velvety-starlight-bdc5ea.netlify.app/" target="_blank" className="btn btn-light mx-2" style={{ background: '#d63384', color: 'white' }}> Deploy </a>
+                    <a href="https://github.com/Victoria00000/E-COMMERCE-APP" target="_blank" rel='noopener no referrer' className="btn btn-light mx-2" style={{ background: '#d63384', color: 'white' }}> GitHub </a>
+                    <a href="https://velvety-starlight-bdc5ea.netlify.app/" target="_blank" rel='noopener no referrer' className="btn btn-light mx-2" style={{ background: '#d63384', color: 'white' }}> Deploy </a>
                   </div>
                 </div>
 
@@ -46,8 +46,8 @@ export const Portfolio = () => {
                     <h5 className="card-title"> Coming soon... </h5>
                     <p className="card-text"> FRONTEND proyect with REACT.JS consuming a public API (fakestoreapi.com).
                       And: "dependencies": "axios": "^0.26.1", "bootstrap": "^5.1.3", "font-awesome": "^4.7.0", "node-modules": "^1.0.1", "react": "^17.0.2", "react-dom": "^17.0.2", "react-loading-skeleton": "^3.0.3", "react-redux": "^7.2.6", "react-router": "^6.2.2", "react-router-dom": "^6.2.2", "react-scripts": "5.0.0", "redux": "^4.1.2"</p>
-                    <a href="" target="_blank" className="btn btn-light mx-2" style={{ background: '#d63384', color: 'white' }}> GitHub </a>
-                    <a href="" target="_blank" className="btn btn-light mx-2" style={{ background: '#d63384', color: 'white' }}> Deploy </a>
+                    <a href="#" className="btn btn-light mx-2" style={{ background: '#d63384', color: 'white' }}> GitHub </a>
+                    <a href="#" className="btn btn-light mx-2" style={{ background: '#d63384', color: 'white' }}> Deploy </a>
                   </div>
                 </div>
 
